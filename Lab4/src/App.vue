c82f75c7b8d85bf26705b1d5f8a3291a570b478f
unknown <hytruong19@gmail.com>
2026-03-28 14:09:07 +0700

    asm

---
+++ Lab4/src/App.vue
@@ -0,0 +1,24 @@
+<!-- <script setup> 
+import Hello from './components/Hello.vue'; 
+</script> 
+<template> 
+<Hello></Hello> 
+</template> 
+<style scoped></style> -->
+
+
+  <!-- <script setup> 
+  import Bai4 from './components/Bai4.vue'; 
+  </script> 
+  
+  <template> 
+    <Bai4/> 
+  </template>  -->
+
+    <script setup> 
+  import HelloWorld from './components/HelloWorld.vue'; 
+  </script> 
+  
+  <template> 
+    <HelloWorld/> 
+  </template> 
---
+++ vueproject/src/components/slide4.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Caculator 18-Trương Tấn Gia Hy</h1>
+        <h3>count {{ count }}</h3>
+        <button @click="increment">+</button>
+        <button @click="decrement">-</button>
+        <button @click="reset">reset</button>
+
+    </div>
+    </template>
+    <script setup>
+import { ref } from 'vue'
+const count = ref(0)
+const increment = () => count.value++
+const decrement = () => count.value--
+const reset = () => (count.value = 0)
+</script>
